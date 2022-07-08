6b010b2e1148f585efdb2ec07181edaf5bfcdc8c
Paul <paulmichaelhardman@gmail.com>
2022-07-08 16:16:13 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
+import { CssBaseline, Grid } from "@mui/material";
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
+    </>
+  );
+}
+
+export default App;
